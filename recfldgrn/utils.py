df0e5f1b682f9133e9b4f5133ecd37a43574aca6
Junjie Luo <jjluo@umd.edu>
2023-04-15 11:31:52 -0400

    update utils

--- recfldgrn/utils.py
+++ recfldgrn/utils.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import os
 
-############## <s>
 def generate_grain_vocab_info(s):
     '''
         s: a Series (or a column of a dataframe) of all rec@fld value's grain_list.
@@ -22,7 +21,6 @@ def generate_grain_vocab_info(s):
     v2idx = {v:k for k, v in idx2v.items()}
     
     return {'idx2v': idx2v, 'v2idx': v2idx, 'v2freq': v2freq}
-############## <e>
 
 
 def get_compressed_df(df_rec, full_recfldgrn_name, prefix_ids):
@@ -94,78 +92,3 @@ def get_highorder_input_idx(df, recfield2grain,
 
     df_p = df_Rec.reset_index(drop = True) # p: patient.
     return df_p
-
-
-def traverse(o, tree_types=(list, tuple), index = None, nest_layer = 100):
-    if isinstance(o, tree_types) and nest_layer > 0:
-        for idx, value in enumerate(o):
-            new_index = index + [idx] if type(index) == list else [idx]
-            for subvalue in traverse(value, tree_types, new_index, nest_layer - 1):
-                yield subvalue
-    else:
-        if not isinstance(o, tree_types): 
-            length = None
-        else:
-            length = len(o)
-        yield index, length, o
-
-
-def convert_relational_list_to_numpy(values_list, new_full_recfldgrn, suffix):
-    o = values_list
-    layer_num = len(new_full_recfldgrn.split('-'))
-    layers = new_full_recfldgrn.replace(suffix, '').split('-')
-    # L = [len(values_list)] 
-
-    D = {}
-
-    # (1) from first layer: 0
-    idx = 0
-    layer_parents = layers[:idx + 1]
-    layer_children = layers[idx + 1]
-    len_name = f'{"2".join(layer_parents)}_ln{layer_children}'
-    len_np = np.array(len(values_list))
-    len_shapes = [len_np.max()] # from layer 0, prepare for layer 1. 
-    D[len_name] = len_np
-
-    # (2) from 1 - last one layers
-    for idx in range(1, layer_num - 1):
-        output = list(traverse(o, nest_layer = idx))
-        # print(output)
-        # data = np.zeros(L)
-        # print('\n\n')
-        # print(idx)
-        # print(output)
-
-        layer_parents = layers[:idx + 1]
-        layer_children = layers[idx + 1]
-        len_name = f'{"-".join(layer_parents)}_ln{layer_children}'
-        # print(len_name)
-
-        locidx  = [i[0] for i in output]
-        length = [i[1] for i in output]
-        # values = [i[2] for i in output]
-
-        len_np = np.zeros(len_shapes).astype(int)
-        # print(len_np.shape, '<---- len_np.shape')
-        for locidx, length, _ in output:
-            len_np[tuple(locidx)] = int(length)
-        # print(len_np)
-        len_shapes.append(len_np.max())
-        # print(len_shapes, '<---- next len_np.shape')
-        # print(length)
-        # print()
-        D[len_name] = len_np
-
-    # (3) for the data
-    idx = layer_num
-    name = new_full_recfldgrn
-    data = np.zeros(len_shapes) # don't convert it to int for now. 
-    output = list(traverse(o, nest_layer = idx))
-    for locidx, _, value in output:
-        data[tuple(locidx)] = value
-        # print(locidx, value)
-        # print(data[tuple(locidx)])# = value
-    data.shape
-    D[name] = data.astype(int)
-    
-    return D
