e43b6899f0b0eba870a99fbf64667a87ab69dcd0
Junjie Luo <jjluo@umd.edu>
2023-05-12 11:15:45 -0400

    update recfldgrn folder

--- recfldgrn/grains/cate/pnsectnm_grn.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def get_SectNmDftGrn_list(x):
-    if pd.isna(x):
-        return ['_missing']
-    else:
-        return x.split('/')
-    
--- recfldgrn/grains/cate/smoking_grn.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from ._base import process_input_str
-
-def func_convert_Smokinggrn(x):
-    if pd.isna(x):
-        return ['_missing']
-    else:
-        return [x]
