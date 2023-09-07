ca630f96b7fd7d1f8baf30874dc5dcd51c589ca7
Junjie Luo <jjluo@umd.edu>
2023-09-07 11:25:03 -0400

    update rfg for setup

--- _static/grains/cate/ecbasic_grn.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def get_basicInfoGrn_list(x):
-    if pd.isna(x):
-        return ['_missing']
-    else:
-        return x.split('&')
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='recfldgrn',
+    version='0.0.1',
+    description='Your package description',
+    author='Your Name',
+    author_email='junjie.luo.jhu@gmail.com',
+    packages=find_packages(),  # Automatically find all packages
+)
